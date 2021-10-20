e3382315484c53c5280c7ac6fd29aaf7fe6617e9
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2021-10-20 15:46:37 +0800

    Update main.py

--- source/ch2/main.py
+++ source/ch2/main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 import logging
-from binance_futures import write_log
 
 
 logger = logging.getLogger()
@@ -19,14 +18,7 @@ file_handler.setLevel(logging.DEBUG)
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
-logger.debug("This message is important only when debugging the program")
-logger.info("This message just shows basic information")
-logger.warning("This message is about something you should pay attention to")
-logger.error("This message helps to debug an error that occured in your program")
-
-write_log()
-
 if __name__ == '__main__':
-
+    
     root = tk.Tk()
     root.mainloop()
