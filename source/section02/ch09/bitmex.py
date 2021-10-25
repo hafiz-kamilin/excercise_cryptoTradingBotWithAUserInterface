d64e6428596a83fb689155b1fe1bc86ce7707b84
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2021-10-25 22:29:29 +0800

    Update bitmex.py

--- source/section02/ch09/bitmex.py
+++ source/section02/ch09/bitmex.py
@@ -6,7 +6,6 @@ def get_contracts():
     response_object = requests.get("https://www.bitmex.com/api/v1/instrument/active")
 
     for contract in response_object.json():
-        
         contracts.append(contract['symbol'])
 
     return contracts
