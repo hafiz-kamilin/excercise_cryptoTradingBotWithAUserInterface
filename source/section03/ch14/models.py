40c865894143c589e82966d4ee92bbb7a221724e
Mohd Hafizuddin Kamilin <hafizkamilin@outlook.com>
2021-11-09 23:23:58 +0800

    Update models.py

--- source/section03/ch14/models.py
+++ source/section03/ch14/models.py
@@ -1,6 +1,7 @@
 class Balance:
 
     def __init__(self, info):
+
         self.initial_margin = float(info['initialMargin'])
         self.maintenance_margin = float(info['maintMargin'])
         self.margin_balance = float(info['marginBalance'])
@@ -10,6 +11,7 @@ class Balance:
 class Candle:
 
     def __init__(self, candle_info):
+
         self.timestamp = candle_info[0]
         self.open = float(candle_info[1])
         self.high = float(candle_info[2])
@@ -20,6 +22,7 @@ class Candle:
 class Contract:
 
     def __init__(self, contract_info):
+
         self.symbol = contract_info['symbol']
         self.base_asset = contract_info['baseAsset']
         self.quote_asset = contract_info['quoteAsset']
@@ -29,6 +32,7 @@ class Contract:
 class OrderStatus:
     
     def __init__(self, order_info):
+        
         self.order_id = order_info['orderId']
         self.status = order_info['status']
         self.avg_price = float(order_info['avgPrice'])
